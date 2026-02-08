55eb9c0dc07e6c0dd7c87184e1c9bac42ef0c63f
ShreyasVH <shreyas.deve.1@gmail.com>
2026-02-08 10:29:31 +0530

    support for doughnut chart

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { BarChart } from "./components/bar-chart/bar-chart";
+import { DoughnutChart } from "./components/doughnut-chart/doughnut-chart";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [BarChart],
+  imports: [BarChart, DoughnutChart, CommonModule],
 })
-export class App {}
+export class App {
+  type = 'bar';
+
+  ngOnInit(): void {
+    const params = new URLSearchParams(window.location.search);
+    this.type = params.get('type') ?? 'bar';
+    console.log(this.type);
+  }
+}
